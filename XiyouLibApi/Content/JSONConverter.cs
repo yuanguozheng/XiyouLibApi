69247097b164dac1ace07bffe8ba5e2030cce335
Yuan Guozheng <yuanguozheng@msn.cn>
2014-06-23 00:38:38 +0800

    Add Some Converters
    JSON, XML

---
+++ XiyouLibApi/Content/JSONConverter.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Open Web API for Xi'an University Of Posts & Telecommunications Library.
+ * 
+ * Copyright (C) 2014 Xiyou Mobile Application Club Microsoft Technology Team All Rights Reserved.
+ * 
+ * File Description: Class for Serializing/Deserializing obj and json string.
+ * 
+ * Created By Yuan Guozheng 2014/6/23.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace XiyouLibApi.Content
+{
+    public class JSONConverter
+    {
+        /// <summary>
+        /// Translate object to JSON String.
+        /// </summary>
+        /// <param name="obj">Object.</param>
+        /// <returns>JSON string.</returns>
+        public static string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        /// <summary>
+        /// Get object from JSON string, support object type only.
+        /// </summary>
+        /// <param name="json">JSON string using object.</param>
+        /// <returns>JObject</returns>
+        public static JObject Deserialize(string json)
+        {
+            return JObject.Parse(json);
+        }
+    }
+}
---
+++ XiyouLibApi/Content/XMLConverter.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Open Web API for Xi'an University Of Posts & Telecommunications Library.
+ * 
+ * Copyright (C) 2014 Xiyou Mobile Application Club Microsoft Technology Team All Rights Reserved.
+ * 
+ * File Description: Class for Serializing/Deserializing obj and XML string.
+ * 
+ * Created By Yuan Guozheng 2014/6/23.
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XiyouLibApi.Content
+{
+    public class XMLConverter
+    {
+        /// <summary>
+        /// Translate object to XML String.
+        /// </summary>
+        /// <param name="obj">Object.</param>
+        /// <returns>XML string.</returns>
+        public static string Serialize(Type type,object obj)
+        {
+            
+        }
+
+        /// <summary>
+        /// Get object from XML string.
+        /// </summary>
+        /// <param name="XML">XML string.</param>
+        /// <returns>Object</returns>
+        public static object Deserialize(string XML)
+        {
+            
+        }
+    }
+}
